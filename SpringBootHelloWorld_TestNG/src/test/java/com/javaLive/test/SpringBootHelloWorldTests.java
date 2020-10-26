26866578661ea49cd710a7c2d648a807781537f3
Administrator <Administrator@DELL>
2020-10-26 16:46:23 +0530

    Some more packages are renamed.

--- SpringBootHelloWorld_TestNG/src/test/java/com/javaLive/test/SpringBootHelloWorldTests.java
+++ SpringBootHelloWorld_TestNG/src/test/java/com/javaLive/test/SpringBootHelloWorldTests.java
@@ -1,4 +1,4 @@
-package com.javainuse.test;
+package com.javaLive.test;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
